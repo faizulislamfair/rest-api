812258f3ecbbc9b030c70a5996e4c8fd24df2d1b
faizulislamfair <faizulislamfair@gmail.com>
2022-06-20 20:32:29 +0600

    asynchandlers

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,39 +1,41 @@
+const asyncHandler = require('express-async-handler')
+
 // Get goals 
 // @route GET /api/goals
  
- const getGoals = async (req, res) => {
+ const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Get goals' })
- }
+ })
 
 
 // Create goal 
 // @route POST /api/goals
  
 
- const createGoal = async (req, res) => {
+ const createGoal = asyncHandler(async (req, res) => {
     if(!req.body.text) {
        res.status(400)
        throw new Error('Add a text field')
     }
 
     res.status(200).json({ message: 'Create goal' })
- }
+ })
 
 
 // Update goal 
 // @route PUT /api/goals/:id
  
- const updateGoal = async (req, res) => {
+ const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update goal ${req.params.id}` })
- }
+ })
 
 
 // Delete goal
 // @route DELETE /api/goals/:id
  
- const deleteGoal = async (req, res) => {
+ const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete goal ${req.params.id}` })
- }
+ })
 
 
 
