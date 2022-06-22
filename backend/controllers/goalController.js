e93b41bbe6262f44c338948cae91913269ec4b69
faizulislamfair <faizulislamfair@gmail.com>
2022-06-22 21:17:12 +0600

    model & goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,10 +1,14 @@
 const asyncHandler = require('express-async-handler')
 
+const Goal = require('../models/goalModel')
+
 // Get goals 
 // @route GET /api/goals
  
  const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get goals' })
+    const goals = await Goals.find()
+
+    res.status(200).json(goals)
  })
 
 
@@ -18,7 +22,11 @@ const asyncHandler = require('express-async-handler')
        throw new Error('Add a text field')
     }
 
-    res.status(200).json({ message: 'Create goal' })
+    const goal = await Goal.create({
+      text: req.body.text
+    })
+
+    res.status(200).json(goal)
  })
 
 
@@ -26,7 +34,18 @@ const asyncHandler = require('express-async-handler')
 // @route PUT /api/goals/:id
  
  const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+      res.status(400)
+      throw new Error('Goal not found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
+
+    res.status(200).json(updateGoal)
  })
 
 
@@ -34,7 +53,16 @@ const asyncHandler = require('express-async-handler')
 // @route DELETE /api/goals/:id
  
  const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete goal ${req.params.id}` })
+   const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+      res.status(400)
+      throw new Error('Goal not found')
+    }
+
+    await goal.remove()
+
+    res.status(200).json({ id: req.params.id })
  })
 
 
---
+++ backend/models/goalModel.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const goalSchema = mongoose.Schema({
+    text: {
+        type: String,
+        require: [true, 'Add a text value']
+    }
+}, {
+    timestamps: true,
+   }
+)
+
+module.exports = mongoose.model('Goal', goalSchema)
